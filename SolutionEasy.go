ed98740213f19c0b528f8ee6d1365ae8a5305bae
vitalii.b <kiverak@gmail.com>
2025-08-20 22:36:07 +0300

    isSameTree

--- SolutionEasy.go
+++ SolutionEasy.go
@@ -117,3 +117,15 @@ func climbStairs(n int) int {
 	}
 	return sum
 }
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
