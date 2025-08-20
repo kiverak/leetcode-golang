1e0615be41be01781d557ecf9c2832adb6dc6143
vitalii.b <kiverak@gmail.com>
2025-08-20 21:54:23 +0300

    init SolutionEasy

---
+++ SolutionEasy.go
@@ -0,0 +1,57 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	indexMap := make(map[int]int)
+	for i, num := range nums {
+		diff := target - num
+		if j, found := indexMap[diff]; found {
+			return []int{j, i}
+		}
+		indexMap[num] = i
+	}
+	return nil
+}
+
+func isValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	var stack []rune
+	for _, char := range s {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')', ']', '}':
+			if len(stack) == 0 {
+				return false
+			}
+			lastOpen := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if (char == ')' && lastOpen != '(') ||
+				(char == ']' && lastOpen != '[') ||
+				(char == '}' && lastOpen != '{') {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func isValid2(s string) bool {
+	stack := []rune{}
+	hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
+
+	for _, char := range s {
+		if match, found := hash[char]; found {
+			if len(stack) > 0 && stack[len(stack)-1] == match {
+				stack = stack[:len(stack)-1] // Pop
+			} else {
+				return false // Invalid
+			}
+		} else {
+			stack = append(stack, char) // Push opening bracket
+		}
+	}
+	return len(stack) == 0
+}
