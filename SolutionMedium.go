3faca3425c180de34dc7e02a7f7bd0e0688026ee
Vitaliy Borodin <kiverak@gmail.com>
2025-08-31 11:32:02 +0300

    dailyTemperatures

--- SolutionMedium.go
+++ SolutionMedium.go
@@ -40,3 +40,38 @@ func lengthOfLongestSubstringRunes(s string) int {
 	}
 	return maxLen
 }
+
+func dailyTemperatures(temperatures []int) []int {
+	res := make([]int, len(temperatures))
+	stack := make([]int, len(temperatures))
+	top := -1
+	for i := range temperatures {
+		for top > -1 && temperatures[i] > temperatures[stack[top]] {
+			res[stack[top]] = i - stack[top]
+			top--
+		}
+		top++
+		stack[top] = i
+	}
+	return res
+}
+
+func dailyTemperatures2(temperatures []int) []int {
+	ans, n := make([]int, len(temperatures)), len(temperatures)
+	hottest := 0
+	for i := n - 1; i >= 0; i-- {
+		if temperatures[i] >= hottest {
+			hottest = temperatures[i]
+			continue
+		}
+
+		days := 1
+		for temperatures[i] >= temperatures[i+days] {
+			days += ans[i+days]
+		}
+
+		ans[i] = days
+	}
+
+	return ans
+}
